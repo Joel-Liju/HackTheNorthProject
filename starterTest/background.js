a30a76a282353d43e99a29c28556f1788879b007
Rod <rodovj29@icloud.com>
2021-01-16 10:05:48 -0500

    added menu bar. There's difference in the manifest file.

--- starterTest/background.js
+++ starterTest/background.js
@@ -1,3 +1,15 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.executeScript(tab.id, { code: 'window.print();' });
+
+var pageConditions = {
+  conditions: [
+    new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: { schemes: ['https','http'] }
+    })
+  ],
+  actions: [new chrome.declarativeContent.ShowPageAction()]
+}
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([pageConditions]);
+  });
 });
---
+++ starterTest/bar.js
@@ -0,0 +1,34 @@
+var body = document.getElementsByTagName("body")[0];
+
+// the menu bar
+var bar = document.createElement("div");
+bar.style.left = 0 + "px";
+bar.style.top = 10 + "px";
+bar.setAttribute("class", "sidebar");
+bar.setAttribute("id", "1");
+body.prepend(bar);
+
+// create a tab on the side of the bar menu to be able to close the bar menu
+var tab = document.createElement("button");
+tab.setAttribute("class", "bartab");
+tab.style.left = "12em";
+bar.prepend(tab);
+
+// create the tab that will be shown when the menu is closed
+var closedTab = document.createElement("button");
+closedTab.setAttribute("class", "bartab");
+closedTab.style.left = "0px";
+closedTab.style.visibility = "hidden";
+body.prepend(closedTab);
+
+// functionality to close the menu
+tab.onclick = () => {
+    document.getElementById("1").style.visibility = "hidden";
+    closedTab.style.visibility = "visible";
+}
+
+// functionality to open the menu back up
+closedTab.onclick = () => {
+    document.getElementById("1").style.visibility = "visible";
+    closedTab.style.visibility = "hidden";
+}
